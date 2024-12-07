590a75543c59478bd528241784a60fa9e661a9df
Poopmaster <124159663+poiqazwsx@users.noreply.github.com>
2024-12-07 18:26:45 +0000

    Create auth.py
    this is trash ill fix it later

---
+++ cogs/utility/auth.py
@@ -0,0 +1,37 @@
+# cogs/untility/auth.py
+# this is cancer rn ill fix it later lol
+import discord
+from discord.ext import commands
+authorized_users = [9366731394129664414, 2987654321098765432] 
+authorized_roles = [1198707036070871102, 222222222222222222]  
+
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+class Auth(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    def is_authorized(interaction: discord.Interaction):
+        user = interaction.user
+        if user.id in authorized_users:
+            return True
+        if any(role.id in authorized_roles for role in user.roles):
+            return True
+#    if user.id == interaction.guild.owner_id:
+#        return TrueF
+        return False
+    @commands.command()
+    async def check_auth(self, ctx):
+        if ctx.author.id in authorized_users:
+            await ctx.send("userid auth")
+        elif any(role.id in authorized_roles for role in ctx.author.roles):
+            await ctx.send("role auth")
+        else:
+            await ctx.send("no perms")
+
+
+            
+def setup(bot):
+    print("setting up the auth cog...")
+    bot.add_cog(Auth(bot))
