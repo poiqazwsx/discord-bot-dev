a5bb1d2be35829a3e5f44a909c092d46b053dc63
Poopmaster <124159663+poiqazwsx@users.noreply.github.com>
2024-12-07 15:43:25 +0000

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,44 @@
+#importpgog
+import discord 
+import asyncio
+from discord.ext import commands
+# intents shittt
+intents = discord.Intents.default()
+intents.message_content = True
+bot = commands.Bot(command_prefix="!", intents=intents)
+# fortnite events
+@bot.event
+async def on_ready():
+    try:
+        print(f'Logged in as {bot.user}')
+        if bot.guilds:
+            for guild in bot.guilds:
+                print(f'Server: {guild.name} (ID: {guild.id})') 
+        else:
+            print("Not currently in any servers")
+    except Exception as e:
+        print("skill issue")
+# load cogs
+async def load_cogs():
+    try:
+         bot.load_extension("cogs.testing.message")
+    except Exception as e:
+        print(f"failed to load cog: {e}")
+
+# do stuff yes
+async def main(): 
+    try:
+        await load_cogs()  
+        await bot.start("") 
+    except discord.LoginFailure as e:
+        print(f"Login failed: {e}")
+    except Exception as e:
+        print(f"somethin happen: {e}")
+
+# sdfjlksdjkljlkdfs
+if __name__ == "__main__":
+    asyncio.run(main())
+
+
+
+
