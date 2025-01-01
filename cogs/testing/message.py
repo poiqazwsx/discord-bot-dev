990a482af08ca26667a8c356c9560763735c410f
Poopmaster <124159663+poiqazwsx@users.noreply.github.com>
2025-01-01 13:19:20 +0000

    awaited

--- cogs/testing/message.py
+++ cogs/testing/message.py
@@ -1,8 +1,11 @@
+# cogs/testing/message.py
 from discord.ext import commands
+
 
 class Message(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
 
     @commands.Cog.listener()
     async def on_message(self, message):
@@ -12,6 +15,8 @@ class Message(commands.Cog):
         if "test" in message.content.lower():
             await message.channel.send("test")
 
-def setup(bot):
+
+
+async def setup(bot):
     print("setting up the message cog...")
-    bot.add_cog(Message(bot))
+    await bot.add_cog(Message(bot))
