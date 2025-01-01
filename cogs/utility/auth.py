45dd165e7e9e79027f3ff44b0f21022862abc868
Poopmaster <124159663+poiqazwsx@users.noreply.github.com>
2025-01-01 13:19:49 +0000

    awaited

--- cogs/utility/auth.py
+++ cogs/utility/auth.py
@@ -1,9 +1,8 @@
 # cogs/untility/auth.py
-# this is cancer rn ill fix it later lol
 import discord
 from discord.ext import commands
-authorized_users = [9366731394129664414, 2987654321098765432] 
-authorized_roles = [1198707036070871102, 222222222222222222]  
+authorized_users = [9366731394129664414, 2987654321098765432]
+authorized_roles = [1198707036070871102, 222222222222222222]
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -31,7 +30,7 @@ class Auth(commands.Cog):
             await ctx.send("no perms")
 
 
-            
-def setup(bot):
+
+async def setup(bot):
     print("setting up the auth cog...")
-    bot.add_cog(Auth(bot))
+    await bot.add_cog(Auth(bot))
