83da6a5387c315d2dd8e427e0bd6e10cca659aec
Poopmaster <124159663+poiqazwsx@users.noreply.github.com>
2025-01-03 01:32:37 +0000

    the cog is
    taking over

--- bot.py
+++ bot.py
@@ -66,6 +66,7 @@ async def load_cogs():
           await bot.load_extension("cogs.utility.fetch")
           await bot.load_extension("cogs.utility.reminder")
           await bot.load_extension("cogs.utility.prompt_changer")
+          await bot.load_extension("cogs.utility.temp_changer")
           await bot.load_extension("cogs.utility.ping")
     except Exception as e:
         logging.error(f"failed to load cog: {e}")
